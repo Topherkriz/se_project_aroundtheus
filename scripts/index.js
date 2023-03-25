3d793b35f3ce26bfc1362f71b3eaf2ab5f7a4dbb
Topherkriz <chris.r.silverio@gmail.com>
2023-03-25 16:30:26 -0400

    new

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,17 @@
-const initialCards1 = "Yosemite Valley";
+const initialCards = "Yosemite Valley";
 ("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg");
 
-const initialCards2 = "Lake Louise";
+const initialCards = "Lake Louise";
 ("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg");
 
-const initialCards3 = "Bald Mountains";
+const initialCards = "Bald Mountains";
 ("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg");
 
-const initialCards4 = "Latemar";
+const initialCards = "Latemar";
 ("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg");
 
-const initialCards5 = "Vanoise National Park";
+const initialCards = "Vanoise National Park";
 ("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg");
 
-const initialCards6 = "Lago di Braies";
+const initialCards = "Lago di Braies";
 ("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg");
