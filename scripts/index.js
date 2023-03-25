7fed2fdbeb4efac2263c4855508e6b3c98793e2d
Topherkriz <chris.r.silverio@gmail.com>
2023-03-25 16:27:37 -0400

    Update

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,17 @@
-const initialCards = "Yosemite Valley"
-"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg";
+const initialCards1 = "Yosemite Valley";
+("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg");
 
-const initialCards = "Lake Louise"
-"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg";
+const initialCards2 = "Lake Louise";
+("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg");
 
-const initialCards = "Bald Mountains"
-"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg";
+const initialCards3 = "Bald Mountains";
+("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg");
 
-const initialCards = "Latemar"
-"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg";
-~~~~
-const initialCards = "Vanoise National Park"
-"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg";
+const initialCards4 = "Latemar";
+("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg");
 
-const initialCards = "Lago di Braies"
-"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg";
+const initialCards5 = "Vanoise National Park";
+("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg");
+
+const initialCards6 = "Lago di Braies";
+("https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg");
