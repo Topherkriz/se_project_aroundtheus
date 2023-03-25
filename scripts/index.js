0980c864f4b4af32bd7c4226e32f8e9b4edab94b
Topherkriz <chris.r.silverio@gmail.com>
2023-03-25 16:18:39 -0400

    Added JS

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+const initialCards = "Yosemite Valley"
+"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg";
+
+const initialCards = "Lake Louise"
+"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg";
+
+const initialCards = "Bald Mountains"
+"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg";
+
+const initialCards = "Latemar"
+"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg";
+~~~~
+const initialCards = "Vanoise National Park"
+"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg";
+
+const initialCards = "Lago di Braies"
+"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg";
