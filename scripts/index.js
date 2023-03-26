94bf4455c35a550b187a0de3846bfb357d5ca4a4
Topherkriz <chris.r.silverio@gmail.com>
2023-03-25 20:18:23 -0400

    added let

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,10 @@
-let initialCards = "Yosemite Valley";
-("../images/yosemite.jpg");
+let initalCards = [
+  { name: "Yosemite Valleye", link: "../images/yosemite.jpg" },
 
-("Lake Louise");
-("../images/lake-louise.jpg");
+  { name: "Lake Louise", link: "../images/lake-louise.jpg" },
 
-("Bald Mountains");
-("../images/bald-mountains.jpg");
-
-("Latemar");
-("../images/latemar.jpg");
-
-("Vanoise National Park");
-("../images/vanoise.jpg");
-
-("Lago di Braies");
-("../images/lago.jpg");
+  { name: "Bald Mountains", link: "../images/bald-mountains.jpg" },
+  { name: "Latemar", link: "../images/latemar.jpg" },
+  { name: "Vanoise National Park", link: "../images/vanoise.jpg" },
+  { name: "Lago di Braiese", link: "../images/lago.jpg" },
+];
