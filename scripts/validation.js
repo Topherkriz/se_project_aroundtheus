4d235ae134176721b07783e1968ff9b53f623056
Topherkriz <chris.r.silverio@gmail.com>
2024-01-03 22:23:20 -0500

    Changes made but still not fully working

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,3 +1,5 @@
+/* 
+
 function showInputError(
   formElement,
   inputElement,
@@ -108,5 +110,55 @@ function enableValidation(config) {
     );
   });
 }
+
+*/
+const showInputError = (input, formEl, { errorClass }) => {
+  const errorSpan = formEl.querySelector("#" + input.id + "-error");
+  errorSpan.textContent = input.validationMessage;
+  input.classList.add(errorClass);
+};
+
+const hideInputError = (input, formEl, { errorClass }) => {
+  const errorSpan = formEl.querySelector("#" + input.id + "-error");
+  errorSpan.textContent = "";
+  input.classList.remove(errorClass);
+};
+
+const checkInputValidity = (formEl, input, settings) => {
+  if (input.validity.valid) {
+    hideInputError(formEl, input, settings);
+  } else {
+    showInputError(formEl, input, settings);
+  }
+};
+
+const setEventListeners = (formEl, settings) => {
+  const inputs = [...formEl.querySelectorAll(settings.inputSelector)];
+  inputs.forEach((input) => {
+    input.addEventListener("input", (e) => {
+      checkInputValidity(formEl, input, settings);
+    });
+  });
+};
+
+const enableValidation = (settings) => {
+  const formElements = [...document.querySelectorAll(settings.formSelector)];
+  formElements.forEach((formEl) => {
+    formEl.addEventListener("submit", (e) => e.preventDefault());
+    setEventListeners(formEl, settings);
+  });
+};
+
+// enabling validation by calling enableValidation()
+// pass all the settings on call
+
+enableValidation({
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "modal__error_visible",
+});
 
 export { enableValidation };
